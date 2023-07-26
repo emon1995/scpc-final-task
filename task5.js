17fb30338302d9aeb607886a8dd82b759f3f8ea7
emon1995 <emonhossain995@gmail.com>
2023-07-26 20:42:06 +0600

    task5 result of the operation

---
+++ task5.js
@@ -0,0 +1,25 @@
+
+function calculator(num1, operator, num2) {
+    switch (operator) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            if (num2 === 0) {
+                return 'Error: Division by zero';
+            }
+            return num1 / num2;
+        default:
+            return 'Error: Invalid operator';
+    }
+}
+
+console.log(calculator(5, '+', 3));
+console.log(calculator(10, '-', 4));
+console.log(calculator(7, '*', 2));
+console.log(calculator(20, '/', 4));
+console.log(calculator(10, '/', 0));
+console.log(calculator(5, '^', 2));   
